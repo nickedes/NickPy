290dc8b5730d3df5cd0bb189f2740e56981d6b12
nikhil <nkmittal4994@gmail.com>
2015-07-31 16:12:59 +0530

    Email:Docstring added.

---
+++ Email/getemail.py
@@ -0,0 +1,13 @@
+def getemails(url):
+	"""Gives a list of email ids found in the given url.
+
+	Parameters
+	----------
+	url : Link of the page from which email ids need to be found.
+
+	Returns
+	-------
+	list
+		A list of email ids.
+	"""
+	pass
