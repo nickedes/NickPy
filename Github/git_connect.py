c6e2816b6a92287c64696e36d7db3b7cae44120f
nikhil <nkmittal4994@gmail.com>
2015-08-02 14:40:13 +0530

    Github:Pep8-ify things! :P

--- Github/git_connect.py
+++ Github/git_connect.py
@@ -1,17 +1,16 @@
 from configparser import ConfigParser
 from github import Github
 
+
 def getPass():
-    """Returns the username and password stored in the config file.
-    """
+    """Returns the username and password stored in the config file."""
     config = ConfigParser()
     config.read('config.ini')
-    user,passwd = config['Github']['user'],config['Github']['pass']
-    return user,passwd
+    user, passwd = config['Github']['user'], config['Github']['pass']
+    return user, passwd
 
 
 def connect():
-    """Creates a Github instance so that we can access Github api.
-    """
-    user,passwd = getPass()
-    return Github(user,passwd)
+    """Creates a Github instance so that we can access Github api."""
+    user, passwd = getPass()
+    return Github(user, passwd)
