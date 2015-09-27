18aa97c0b37e82346cea1f33c65d6d8cc03b1868
nikhil <nkmittal4994@gmail.com>
2015-09-27 23:15:19 +0530

    INput 2 variables in one line....first TIME EVAR!

--- random.py
+++ random.py
@@ -1,6 +1,8 @@
 from time import time
 from itertools import islice
-a, b = 1, 1000000000
+# a, b = 1, 1000000000
+a, b = input().split(' ')
+a, b = int(a), int(b)
 xor = a
 start = time()
 lis = range(b+1)
