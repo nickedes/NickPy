0269751a7c0f9419e4e5f14bf73cb181f24da845
nikhil <nkmittal4994@gmail.com>
2015-10-11 12:37:03 +0530

    Trying to get the mp3 from direct link

--- Talk Python/episodes.py
+++ Talk Python/episodes.py
@@ -1,9 +1,17 @@
 from requests import get
 
-url = 'http://talkpython.fm/episodes/all'
+# url = 'http://talkpython.fm/episodes/all'
 
-test_url = 'http://talkpython.fm/episodes/show/28/making-python-fast-profiling\
--python-code'
+# test_url = 'http://talkpython.fm/episodes/show/28/making-python-fast-profiling\
+# -python-code'
 
-data = get(test_url)
-print(data.content)
+# data = get(test_url)
+# print(data.content)
+
+# Got a direct url for download. Yay! :D
+url = 'http://talkpython.fm/episodes/download/'
+
+for num in range(1, 3):
+    print(num)
+    response = get(url+str(num))
+    print(response.status_code)
