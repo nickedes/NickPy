cbafe4a9d8c09cd9a1823fd10671764b1fe4863d
nikhil <nkmittal4994@gmail.com>
2015-10-11 14:57:26 +0530

    Talk python : downloaded 1st audio :tada: :)

--- Talk Python/episodes.py
+++ Talk Python/episodes.py
@@ -2,16 +2,11 @@ from requests import get
 
 # url = 'http://talkpython.fm/episodes/all'
 
-# test_url = 'http://talkpython.fm/episodes/show/28/making-python-fast-profiling\
-# -python-code'
-
-# data = get(test_url)
-# print(data.content)
-
 # Got a direct url for download. Yay! :D
 url = 'http://talkpython.fm/episodes/download/'
 
-for num in range(1, 3):
-    print(num)
-    response = get(url+str(num))
-    print(response.status_code)
+for num in range(1, 2):
+    response = get(url+str(num)+'/').content
+    filename = "1.mp3"
+    with open(filename, 'wb') as f:
+        f.write(response)
