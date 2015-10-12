a8a7cb42ddebfbf1363115d417c697f7ab043c65
nikhil <nkmittal4994@gmail.com>
2015-10-12 13:25:28 +0530

    Writing data to file... not the response, actually content of the response

--- Talk Python/episodes.py
+++ Talk Python/episodes.py
@@ -6,13 +6,13 @@ from requests import get
 url = 'http://talkpython.fm/episodes/download/'
 location = '/home/nickedes/Music/Talk Python/'
 
-for num in range(2, 30):
+for num in range(1, 30):
     response = get(url+str(num)+'/')
     print(response.status_code)
     # if response.status_code == '200':
     data = response.content
     filename = location + str(num) + '.mp3'
     with open(filename, 'wb') as f:
-        f.write(response)
+        f.write(data)
     # doing this for d first time :P
     print('-Downloaded-', num)
